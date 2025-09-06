c4f7b2cdc4ef1e293ab4e71a8fb6ad428c2aace1
anu1507 <161292778+anu1507@users.noreply.github.com>
2025-09-06 21:16:25 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,28 @@
+const SeatAvailable=document.getElementById("SeatAvailable");
+const ageAbove50=document.getElementById("ageAbove50");
+const hasAadhar=document.getElementById("hasAadharCard");
+const checkButton=document.getElementById("checkstatus");
+const passengerStatus=document.getElementById("status");
+checkButton.addEventListener("click",function(){
+    let isSeatAvailable=SeatAvailable.checked;
+    let isAgeAbove50=ageAbove50.checked;
+    let hasAadharCard=hasAadhar.checked;
+    console.log(isSeatAvailable,isAgeAbove50,hasAadharCard);
+    if (isSeatAvailable){
+        console.log("Seat is Available");
+        if(isAgeAbove50){
+            console.log("Age above50");
+        if(hasAadharCard){
+            console.log("hasAadharCard");
+            alert("please come inside the bus");
+        }else{console.log("but you dont have AadharCard");
+ /*passengerStatus.textContent="seat is Available,age above50,but you dont have AadharCard";*/
+document.getElementById('message').innerText="seat is Available,age above50,but you dont have AadharCard"}         }
+        else{
+    console.log("but your age is below 50 years");
+   passengerStatus.textContent="seat is Available,but your age is below 50 years";}}
+else {
+    console.log("seat is not available");
+    passengerStatus.textContent="seat is not Available";}});
+
+
